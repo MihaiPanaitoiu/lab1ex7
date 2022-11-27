c3817af0765ce0235745e6f1fca99e005763bf48
mihai.panaitoiu <mihai.panaitoiu@softem.ro>
2022-11-27 19:47:38 +0200

    Add project files.

---
+++ lab1ex7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace lab1ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Ex7
+                 Scrieti un program care va verifica daca un numar citit de la tastatura este par sau
+                 impar
+                  In cazul in care numarul este par, programul va afisa “par” iar in caz contrar, “impar”.
+                  Google :even and odd number
+             */
+
+            Console.WriteLine("Acest program va afisa daca numarul este par sau impar");
+
+            Console.WriteLine("Introduceti numarul");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine(num % 2 == 0 ? "par" : "impar");
+
+        }
+    }
+}
